689fe188b667965223bcff7d8eaf9197ab7f7f80
Shimon Doodkin <helpmepro1@gmail.com>
2022-05-23 04:04:23 +0300

    fix monitor class example

--- examples/monitoring/zmq_monitor_class.py
+++ examples/monitoring/zmq_monitor_class.py
@@ -28,32 +28,32 @@ while True:
 # Copyright (C) PyZMQ Developers
 # Distributed under the terms of the Modified BSD License.
 
-import struct
-from typing import List
-
-import zmq
-from zmq.error import _check_version
 import asyncio
 import threading
+import time
+from typing import Any, Dict
 
+import zmq
 from zmq.utils.monitor import recv_monitor_message
 
 
-def event_monitor_thread_async(monitor: zmq.Socket, loop: asyncio.BaseEventLoop) -> None:
-    """ A thread that prints events
-    
-        This is a convenience method. It could serve as an example for your code of a monitor, 
-        For example if you don't need the prints, then copy paste this part of code to your code and modify it to your needs.
-        
-        parameters:
-        
-        monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket() 
-        loop: an asyncio event loop, from calling zmq.asyncio.asyncio.get_event_loop() , whens starting a thread it does not conatins an event loop
+def event_monitor_thread_async(
+    monitor: zmq.Socket, loop: asyncio.BaseEventLoop
+) -> None:
+    """A thread that prints events
+
+    This is a convenience method. It could serve as an example for your code of a monitor,
+    For example if you don't need the prints, then copy paste this part of code to your code and modify it to your needs.
+
+    parameters:
+
+    monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket()
+    loop: an asyncio event loop, from calling zmq.asyncio.asyncio.get_event_loop() , whens starting a thread it does not conatins an event loop
     """
     print("libzmq-%s" % zmq.zmq_version())
     if zmq.zmq_version_info() < (4, 0):
         raise RuntimeError("monitoring in libzmq version < 4.0 is not supported")
-        
+
     EVENT_MAP = {}
     print("Event names:")
     for name in dir(zmq):
@@ -61,11 +61,12 @@ def event_monitor_thread_async(monitor: zmq.Socket, loop: asyncio.BaseEventLoop)
             value = getattr(zmq, name)
             print("%21s : %4i" % (name, value))
             EVENT_MAP[value] = name
-            
+
     print()
     print()
 
     asyncio.set_event_loop(loop)
+
     async def runLoop():
         while True:
             try:
@@ -85,23 +86,25 @@ def event_monitor_thread_async(monitor: zmq.Socket, loop: asyncio.BaseEventLoop)
         monitor.close()
         print()
         print("event monitor thread done!")
+
     asyncio.ensure_future(runLoop())
-   
+
+
 def event_monitor_thread(monitor: zmq.Socket) -> None:
-    """ A thread that prints events
-    
-        This is a convenience method. It could serve as an example for your code of a monitor, 
-        For example if you don't need the prints, then copy paste this part of code to your code and modify it to your needs.
-        
-        parameters:
-        
-        monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket() 
+    """A thread that prints events
+
+    This is a convenience method. It could serve as an example for your code of a monitor,
+    For example if you don't need the prints, then copy paste this part of code to your code and modify it to your needs.
+
+    parameters:
+
+    monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket()
     """
-    
+
     print("libzmq-%s" % zmq.zmq_version())
     if zmq.zmq_version_info() < (4, 0):
         raise RuntimeError("monitoring in libzmq version < 4.0 is not supported")
-        
+
     EVENT_MAP = {}
     print("Event names:")
     for name in dir(zmq):
@@ -109,7 +112,7 @@ def event_monitor_thread(monitor: zmq.Socket) -> None:
             value = getattr(zmq, name)
             print("%21s : %4i" % (name, value))
             EVENT_MAP[value] = name
-            
+
     print()
     print()
 
@@ -130,41 +133,42 @@ def event_monitor_thread(monitor: zmq.Socket) -> None:
     monitor.close()
     print()
     print("event monitor thread done!")
-    
-    
+
+
 def event_monitor(socket: zmq.Socket) -> None:
-    """ Add printing event monitor to a zmq socket, it creates a thread by calling event_monitor_thread
-    
-        usage:
+    """Add printing event monitor to a zmq socket, it creates a thread by calling event_monitor_thread
 
-        event_monitor_async(socket)
+    usage:
 
-        parameters:
-        
-        monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket() 
-        
+    event_monitor_async(socket)
+
+    parameters:
+
+    monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket()
+
     """
     monitor = socket.get_monitor_socket()
     t = threading.Thread(target=event_monitor_thread, args=(monitor,))
     t.start()
-    
-def event_monitor_async(socket: zmq.Socket, loop: asyncio.BaseEventLoop) -> None:
-    """ Add printing event monitor to a zmq socket, it creates a thread by calling event_monitor_thread_async
-    
-        See notes in description of : event_monitor_thread
-        
-        usage:
-            loop = zmq.asyncio.asyncio.get_event_loop()
-            event_monitor_async(socket, zmq.asyncio.asyncio.get_event_loop() )
 
-        parameters:
-        
-        monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket() 
+
+def event_monitor_async(socket: zmq.Socket, loop: asyncio.BaseEventLoop) -> None:
+    """Add printing event monitor to a zmq socket, it creates a thread by calling event_monitor_thread_async
+
+    See notes in description of : event_monitor_thread
+
+    usage:
+        loop = zmq.asyncio.asyncio.get_event_loop()
+        event_monitor_async(socket, zmq.asyncio.asyncio.get_event_loop() )
+
+    parameters:
+
+    monitor: a zmq monitor socket, from calling:  my_zmq_socket.get_monitor_socket()
     """
-  
+
     monitor = socket.get_monitor_socket()
-    t = threading.Thread(target=event_monitor_thread_async, args=(monitor,loop))
+    t = threading.Thread(target=event_monitor_thread_async, args=(monitor, loop))
     t.start()
 
-__all__ = ['event_monitor', 'event_monitor_async']
 
+__all__ = ['event_monitor', 'event_monitor_async']
